6a7c1363ba41a80f6bd9ec562f9dcd2e91d7cced
Tarun Kumar Ghorui <tghorui@armor.com>
2020-02-27 12:20:06 +0530

    BArCode with MRP

--- InventoryApp/Controllers/HomeController.cs
+++ InventoryApp/Controllers/HomeController.cs
@@ -3,6 +3,7 @@ using InventoryApp.Models.BarCode;
 using System;
 using System.Collections.Generic;
 using System.Drawing;
+using System.Globalization;
 using System.Web.Mvc;
 
 namespace InventoryApp.Controllers
@@ -62,16 +63,30 @@ namespace InventoryApp.Controllers
 
             var barCodeValue = dto.BarCodeValue;
             var dir = Server.MapPath("/Images/BarCode");
+            string tempFilePath = dir + "/temp.jpg";
             string fileName = DateTime.Now.ToString("ddMMyyyyHHmmss") + ".jpg";
             string filePath = dir + "/" + fileName;
             string relativePath = "/Images/BarCode/" + fileName;
-            
-            BarcodeLib.Barcode b = new BarcodeLib.Barcode();
-            b.IncludeLabel = true;
+            BarcodeLib.Barcode b = new BarcodeLib.Barcode {IncludeLabel = true};
             Image img = b.Encode(BarcodeLib.TYPE.CODE128, barCodeValue, Color.Black, Color.White, 290, 120);
-            img.Save(filePath);
-            
-            List<string> result = new List<string>() { barCodeValue, relativePath };
+            img.Save(tempFilePath);
+            PointF firstLocation = new PointF(0f, 0f);
+            StringFormat drawFormat = new StringFormat { FormatFlags = StringFormatFlags.DirectionVertical };
+
+            Bitmap bitmap = (Bitmap)Image.FromFile(tempFilePath);//load the image file
+
+            using (Graphics graphics = Graphics.FromImage(bitmap))
+            {
+                using (Font arialFont = new Font("Arial", 14))
+                {
+                    graphics.DrawString("MRP: " + dto.Mrp.ToString(CultureInfo.InvariantCulture), arialFont, Brushes.Black, firstLocation, drawFormat);
+                }
+            }
+
+            bitmap.Save(filePath);//save the image file
+            bitmap.Dispose();
+
+            List<string> result = new List<string> { barCodeValue, relativePath };
             return Json(result, JsonRequestBehavior.AllowGet);
         }
 
