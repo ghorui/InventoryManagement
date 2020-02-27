243c68e4af7b390899a89ff279a69b37d3d729d8
Tarun Kumar Ghorui <tghorui@armor.com>
2020-02-28 00:54:28 +0530

    Added barcode print

--- InventoryApp/Controllers/HomeController.cs
+++ InventoryApp/Controllers/HomeController.cs
@@ -2,8 +2,11 @@
 using InventoryApp.Models.BarCode;
 using System;
 using System.Collections.Generic;
+using System.Diagnostics;
 using System.Drawing;
+using System.Drawing.Printing;
 using System.Globalization;
+using System.IO;
 using System.Web.Mvc;
 
 namespace InventoryApp.Controllers
@@ -59,40 +62,76 @@ namespace InventoryApp.Controllers
 
         public JsonResult GetBarCode(BarCodeDTO dto)
         {
-            ValidateBarCodeProperties(dto);
-
-            var barCodeValue = dto.BarCodeValue;
-            var dir = Server.MapPath("/Images/BarCode");
-            string tempFilePath = dir + "/temp.jpg";
-            string fileName = DateTime.Now.ToString("ddMMyyyyHHmmss") + ".jpg";
-            string filePath = dir + "/" + fileName;
-            string relativePath = "/Images/BarCode/" + fileName;
-            BarcodeLib.Barcode b = new BarcodeLib.Barcode {IncludeLabel = true};
-            Image img = b.Encode(BarcodeLib.TYPE.CODE128, barCodeValue, Color.Black, Color.White, 290, 120);
-            img.Save(tempFilePath);
-            PointF firstLocation = new PointF(0f, 0f);
-            StringFormat drawFormat = new StringFormat { FormatFlags = StringFormatFlags.DirectionVertical };
-
-            Bitmap bitmap = (Bitmap)Image.FromFile(tempFilePath);//load the image file
-
-            using (Graphics graphics = Graphics.FromImage(bitmap))
+            try
             {
-                using (Font arialFont = new Font("Arial", 14))
+                ValidateBarCodeProperties(dto);
+
+                var barCodeValue = dto.BarCodeValue;
+                var dir = Server.MapPath("/Images/BarCode");
+                string tempFilePath = dir + "/temp.jpg";
+                string fileName = DateTime.Now.ToString("ddMMyyyyHHmmss") + ".jpg";
+                string filePath = dir + "/" + fileName;
+                string relativePath = "/Images/BarCode/" + fileName;
+                BarcodeLib.Barcode b = new BarcodeLib.Barcode { IncludeLabel = true };
+                Image img = b.Encode(BarcodeLib.TYPE.CODE128, barCodeValue, Color.Black, Color.White, 290, 200);
+                img.Save(tempFilePath);
+
+                PointF firstLocation = new PointF(0f, 0f);
+                PointF secondLocation = new PointF(250f, 0f);
+                StringFormat drawFormat = new StringFormat { FormatFlags = StringFormatFlags.DirectionVertical };
+
+                Bitmap bitmap = (Bitmap)Image.FromFile(tempFilePath);//load the image file
+
+                using (Graphics graphics = Graphics.FromImage(bitmap))
                 {
-                    graphics.DrawString("MRP: " + dto.Mrp.ToString(CultureInfo.InvariantCulture), arialFont, Brushes.Black, firstLocation, drawFormat);
+                    using (Font arialFont = new Font("Arial", 10))
+                    {
+                        graphics.DrawString("MRP: " + dto.Mrp.ToString(CultureInfo.InvariantCulture), arialFont,
+                            Brushes.Black, firstLocation, drawFormat);
+                        graphics.DrawString("Vendor: " + dto.Vendor.ToString(CultureInfo.InvariantCulture), arialFont,
+                            Brushes.Black, secondLocation, drawFormat);
+                    }
+
+                    using (Font arialFont = new Font("Arial", 8))
+                    {
+                        firstLocation = new PointF(0f, 100f);
+                        graphics.DrawString(dto.Craft.ToString(CultureInfo.InvariantCulture), arialFont,
+                            Brushes.Crimson, firstLocation, drawFormat);
+                    }
                 }
+                bitmap.Save(filePath);//save the image file
+                bitmap.Dispose();
+
+                List<string> result = new List<string> { barCodeValue, relativePath };
+                Console.WriteLine($"Home/GetBarCode. result: {result}");
+                return Json(result, JsonRequestBehavior.AllowGet);
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e);
+                throw;
             }
 
-            bitmap.Save(filePath);//save the image file
-            bitmap.Dispose();
-
-            List<string> result = new List<string> { barCodeValue, relativePath };
-            return Json(result, JsonRequestBehavior.AllowGet);
         }
 
         private void ValidateBarCodeProperties(BarCodeDTO dto)
         {
             BarCodeBLL.ValidateBarCodeProperties(dto);
+        }
+
+        public JsonResult BarCodeToPrinter(string filePath)
+        {
+            var dir = Server.MapPath("/Images/BarCode");
+            string path = dir + filePath.Substring(filePath.IndexOf("BarCode") + 7);
+            Process p = new Process
+            {
+                StartInfo = new ProcessStartInfo()
+                {
+                    CreateNoWindow = true, Verb = "print", FileName = path //put the correct path here
+                }
+            };
+            p.Start();
+            return Json(true);
         }
     }
 }
